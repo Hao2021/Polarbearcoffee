0375a8fe9fce3d80585bc4747cf9c0bc63f13bed
Hao2021 <33287196+Hao2021@users.noreply.github.com>
2021-04-16 14:54:44 +0700

    ANother commit

---
+++ build/precache-manifest.7e0028b13e324c7438e94bbf0be709d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56c5430dc4e452162054e4c1c7d179a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "288f5f905e7ec298a1f2",
+    "url": "/static/css/main.631deffa.chunk.css"
+  },
+  {
+    "revision": "36128ff7f373e5beff7a",
+    "url": "/static/js/2.1442e098.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.1442e098.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "288f5f905e7ec298a1f2",
+    "url": "/static/js/main.0abacfbd.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  }
+]);
